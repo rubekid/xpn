32559ff157f31ec7ca3bfd5af377f613343b5844
杨立哲 <562618993@qq.com>
2018-10-18 10:17:11 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,17 @@
+import './lib/common/common'
+
+import Http from './lib/http/http'
+
+import AjaxCache from './lib/cache/cache'
+import Storage from './lib/cache/storage'
+import Session from './lib/cache/session'
+
+import DateUtil from './lib/utils/date-util'
+
+export {
+  Http,
+  AjaxCache,
+  Storage,
+  Session,
+  DateUtil
+}
